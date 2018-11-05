3349496040be99c9d99aa92de092d2ec480667be
Pato Martinez <jpmartinez91@gmail.com>
2018-11-04 19:01:30 -0500

    first commit

---
+++ handle-aws.js
@@ -0,0 +1,30 @@
+'use strict';
+const exec = require('child_process').exec;
+const execSync = require('child_process').execSync;
+
+const tload = process.hrtime();
+
+const hello = (event, context, callback) =>
+{
+    const macAddr = execSync('/sbin/ip link | grep "link-netnsid 0" | cut -f 6 -d " " | tr -d "[:space:]"').toString()
+    const cpuModel = execSync('grep "model name" /proc/cpuinfo | head -n 1 | cut -f 2 -d : | xargs').toString()
+    callback(null, {
+        statusCode: 200,
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+        },
+        body: JSON.stringify({
+            time: new Date().getTime(),
+            mac: macAddr,
+            cpu: cpuModel,
+            time: process.hrtime(tload)
+        })
+    })
+};
+
+
+module.exports.hello128 = hello;
+module.exports.hello256 = hello;
+module.exports.hello512 = hello;
+module.exports.hello1024 = hello;
+module.exports.hello2048 = hello;
