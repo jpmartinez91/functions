e73bce159d5526621e85038c4e0f81b26bb32ccd
Pato Martinez <jpmartinez91@gmail.com>
2018-11-05 16:22:18 -0500

    complete others providers

--- google/index.js
+++ google/index.js
@@ -1,9 +1,30 @@
 'use strict';
+const os = require("os")
+const tload = process.hrtime();
 
-exports.http = (request, response) => {
-  response.status(200).send('Hello World!');
+const hello = (request, response) =>
+{
+
+  response.status(200).send({
+    time: new Date().getTime(),
+    cpu: os.cpus(),
+    address: os.networkInterfaces(),
+    time_procces: process.hrtime(tload),
+    memory: os.totalmem(),
+    free_memory: os.freemem(),
+    platform: os.platform(),
+    os_release: os.release(),
+    os_type: os.type()
+  });
 };
 
-exports.event = (event, callback) => {
+exports.event = (event, callback) =>
+{
   callback();
 };
+
+module.exports.hello128 = hello;
+module.exports.hello256 = hello;
+module.exports.hello512 = hello;
+module.exports.hello1024 = hello;
+module.exports.hello2048 = hello;
--- ibm/handler.js
+++ ibm/handler.js
@@ -1,8 +1,23 @@
 'use strict';
-
-function hello(params) {
-  const name = params.name || 'World';
-  return { payload: `Hello, ${name}!` };
+const os = require("os")
+const tload = process.hrtime();
+const hello = (params) =>
+{
+  return {
+    time: new Date().getTime(),
+    cpu: os.cpus(),
+    address: os.networkInterfaces(),
+    time_procces: process.hrtime(tload),
+    memory: os.totalmem(),
+    free_memory: os.freemem(),
+    platform: os.platform(),
+    os_release: os.release(),
+    os_type: os.type()
+  }
 }
 
-exports.hello = hello;
+module.exports.hello128 = hello;
+module.exports.hello256 = hello;
+module.exports.hello512 = hello;
+module.exports.hello1024 = hello;
+module.exports.hello2048 = hello;
